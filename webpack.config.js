bf21fb51f1e72b9192529b4e8d3a8913da523ef6
adeyem1 <adeyemiadesanya399@gmail.com>
2024-03-19 22:41:35 +0100

    almost done with the front

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  entry: './app.js', // Updated path to the JavaScript entry file
+  output: {
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  }
+};
