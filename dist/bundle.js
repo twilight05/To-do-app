52a2c688ed3abe6d75f0f8948dc073eac36fa5cc
adeyem1 <adeyemiadesanya399@gmail.com>
2024-03-20 10:17:22 +0100

    okayyyyy

--- dist/bundle.js
+++
@@ -1,8 +0,0 @@
-// TW Elements is free under AGPL, with commercial license required for specific uses. See more details: https://tw-elements.com/license/ and contact us for queries at tailwind@mdbootstrap.com 
-// Initialization for ES Users
-import {
-    Tab,
-    initTWE,
-  } from "tw-elements";
-  
-  initTWE({ Tab });
